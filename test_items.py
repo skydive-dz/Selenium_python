5edb04dae45f821a4107ba1efed26e00ce34c43e
skydive-dz <sn7363@mail.ru>
2021-10-07 14:29:49 +0300

    Add conftest.py and test_items.py

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+def test_user_should_find_busket_button(browser):
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+	browser.get(link)
+	# time.sleep(10)
+	browser.implicitly_wait(10)
+	assert browser.find_element_by_css_selector(".add-to-basket .btn"), "Button is not defined"
+	

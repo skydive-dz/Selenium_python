5b2292ef28752dda1dfd6826b9e46981d0d0d673
skydive-dz <sn7363@mail.ru>
2021-10-09 10:29:29 +0300

    Modified test

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 def test_user_should_find_busket_button(browser):
 	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 	browser.get(link)
-	# time.sleep(10)
+	time.sleep(3)
 	browser.implicitly_wait(10)
 	assert browser.find_element_by_css_selector(".add-to-basket .btn"), "Button is not defined"
 	
